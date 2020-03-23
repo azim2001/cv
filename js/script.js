bcfe473da33bda3196ae0793ec79bc2537645ad3
azim2001 <centrsistema@bk.ru>
2020-03-23 21:35:31 +0500

    init commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const checkbox = document.querySelector('.theme-switch__checkbox');
+const cards = document.querySelectorAll('.card');
+const transition = () => {
+  document.documentElement.classList.add('transition');
+  setTimeout(() => document.documentElement.classList.remove('transition'), 2000);
+}
+
+checkbox.addEventListener('change', () => {
+  transition();
+  if (checkbox.checked) {
+  	document.documentElement.setAttribute('data-theme', 'dark')
+  	cards.forEach(card => card.classList.add('bg-dark'))
+  } else {
+  	document.documentElement.setAttribute('data-theme', 'light')
+  	cards.forEach(card => card.classList.remove('bg-dark'))
+  }
+})
