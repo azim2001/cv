a9581271dc0c45aac18ddc029c8ff7103ffb89c2
azim2001 <centrsistema@bk.ru>
2020-03-30 07:33:10 +0500

    removed horizontal scroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const checkbox = document.querySelector('.theme-switch__checkbox');
 const cards = document.querySelectorAll('.card');
+const tech = document.getElementById('tech')
 const transition = () => {
   document.documentElement.classList.add('transition');
   setTimeout(() => document.documentElement.classList.remove('transition'), 2000);
@@ -10,8 +11,10 @@ checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
   	document.documentElement.setAttribute('data-theme', 'dark')
   	cards.forEach(card => card.classList.add('bg-dark'))
+    tech.classList.add('bg-dark')
   } else {
   	document.documentElement.setAttribute('data-theme', 'light')
   	cards.forEach(card => card.classList.remove('bg-dark'))
+    tech.classList.remove('bg-dark')
   }
 })
