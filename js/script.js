2d5dbcaf0bc9526e913d59876471ccec6a555fd6
azim2001 <centrsistema@bk.ru>
2020-07-01 17:22:20 +0500

    adding footer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const checkbox = document.querySelector('.theme-switch__checkbox');
 const cards = document.querySelectorAll('.card');
-const tech = document.getElementById('tech')
+const tech = document.getElementById('tech');
+const footer = document.querySelector('.footer');
+const footerParagraph = document.querySelector('.follow-list+p');
+const footerCredits = document.querySelector('.footer-credits p');
+const address = document.querySelector('.location');
+const followItems = document.querySelectorAll('.follow-list-item a i')
+
+
 const transition = () => {
   document.documentElement.classList.add('transition');
   setTimeout(() => document.documentElement.classList.remove('transition'), 2000);
@@ -12,9 +19,19 @@ checkbox.addEventListener('change', () => {
   	document.documentElement.setAttribute('data-theme', 'dark')
   	cards.forEach(card => card.classList.add('bg-dark'))
     tech.classList.add('bg-dark')
+    footer.classList.add('bg-black')
+    footerParagraph.classList.add('text-white')
+    footerCredits.classList.add('text-white')
+    address.classList.add('text-white')
+    followItems.forEach(item => item.classList.add('text-white'))
   } else {
   	document.documentElement.setAttribute('data-theme', 'light')
   	cards.forEach(card => card.classList.remove('bg-dark'))
     tech.classList.remove('bg-dark')
+    footer.classList.remove('bg-black')
+    footerParagraph.classList.remove('text-white')
+    footerCredits.classList.remove('text-white')
+    address.classList.remove('text-white')
+    followItems.forEach(item => item.classList.remove('text-white'))
   }
 })
